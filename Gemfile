16120fd0263e791474d9a0ee89df46e122a29d22
MICHAEL JASINGH <michaeljasingh@gmail.com>
2015-12-10 17:42:36 +0800

    added facebook login authentication

--- Gemfile
+++ Gemfile
@@ -23,6 +23,13 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+# email & password authentication
+gem 'clearance'
+
+gem 'omniauth-facebook'
+gem 'omniauth'
+gem 'figaro'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,36 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  # get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  # delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  # get "/sign_up" => "clearance/users#new", as: "sign_up"
+  # get 'home/index'
+  root 'home#index'
+
+# get '/users', to: 'users#new'
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+  resources :users, only: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
